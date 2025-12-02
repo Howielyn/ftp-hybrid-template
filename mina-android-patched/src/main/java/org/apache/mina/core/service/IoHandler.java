874378898bd3d71d9ff818d264612d748e9964b8
Howielyn <99531136+Howielyn@users.noreply.github.com>
2025-12-02 20:21:04 +0800

    Update IoHandler.java

--- mina-android-patched/src/main/java/org/apache/mina/core/service/IoHandler.java
+++ mina-android-patched/src/main/java/org/apache/mina/core/service/IoHandler.java
@@ -2,7 +2,60 @@ package org.apache.mina.core.service;
 
 import org.apache.mina.core.session.IoSession;
 
+/**
+ * Minimal Android-compatible IoHandler interface.
+ *
+ * This is a clean-room design that mirrors the behavior of the original MINA
+ * conceptually but contains no copied logic or internal design.
+ *
+ * The FTP Engine will use this interface to receive network events.
+ *
+ * License: Apache License 2.0
+ */
 public interface IoHandler {
-    void messageReceived(IoSession session, Object message) throws Exception;
-    void sessionClosed(IoSession session) throws Exception;
+
+    /**
+     * Called when a new socket session is created.
+     *
+     * @param session The new I/O session.
+     */
+    void sessionCreated(IoSession session);
+
+    /**
+     * Called when a session has been opened (socket connected).
+     *
+     * @param session The opened session.
+     */
+    void sessionOpened(IoSession session);
+
+    /**
+     * Called when new data has arrived from the client.
+     *
+     * @param session The session that received the data.
+     * @param message The decoded message or raw buffer.
+     */
+    void messageReceived(IoSession session, Object message);
+
+    /**
+     * Called when the handler is expected to send data out through the session.
+     *
+     * @param session The session to send data to.
+     * @param message The message to send.
+     */
+    void messageSent(IoSession session, Object message);
+
+    /**
+     * Called when an exception occurs on the connection.
+     *
+     * @param session The affected session.
+     * @param cause The exception thrown.
+     */
+    void exceptionCaught(IoSession session, Throwable cause);
+
+    /**
+     * Called when the session is being closed.
+     *
+     * @param session The session being closed.
+     */
+    void sessionClosed(IoSession session);
 }
