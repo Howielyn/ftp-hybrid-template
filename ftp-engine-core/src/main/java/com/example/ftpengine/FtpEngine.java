810344234602cab17fab99663a0994dad502eb62
Howielyn <99531136+Howielyn@users.noreply.github.com>
2025-12-04 05:50:15 +0800

    Update FtpEngine.java

--- ftp-engine-core/src/main/java/com/example/ftpengine/FtpEngine.java
+++ ftp-engine-core/src/main/java/com/example/ftpengine/FtpEngine.java
@@ -1,6 +1,5 @@
 package com.example.ftpengine;
 
-import org.apache.mina.core.service.IoHandler;
 import org.apache.mina.core.service.NioSocketAcceptor;
 import java.net.InetSocketAddress;
 
@@ -11,8 +10,16 @@ public class FtpEngine {
 
     public FtpEngine(IFtpFileSystem fs) {
         this.processor = new FtpCommandProcessor(fs, new FtpUserManager());
-        IoHandler handler = new FtpIoHandlerAndroid(processor);
-        this.acceptor = new NioSocketAcceptor(handler);
+
+        // Construct acceptor with no arguments
+        this.acceptor = new NioSocketAcceptor();
+
+        // Assign handler separately
+        this.acceptor.setHandler(new FtpIoHandlerAndroid(processor));
+
+        // Optional: configure session buffer sizes
+        this.acceptor.getSessionConfig().setReadBufferSize(1024);
+        this.acceptor.getSessionConfig().setReceiveBufferSize(1024);
     }
 
     public void start(int port) throws Exception {
@@ -21,7 +28,10 @@ public class FtpEngine {
     }
 
     public void stop() {
-        if (acceptor != null) acceptor.shutdown();
+        if (acceptor != null) {
+            acceptor.unbind();
+            acceptor.dispose();
+        }
         System.out.println("FtpEngine stopped");
     }
 
